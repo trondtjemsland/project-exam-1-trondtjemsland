1f757a7d32cc2b3f5c61ad3642a39ac8c1800b70
trondtjemsland <69841794+trondtjemsland@users.noreply.github.com>
2021-05-24 13:33:52 +0200

    s

--- js/details.js
+++ js/details.js
@@ -27,10 +27,9 @@ async function getBlogposts(Id) {
 
     const openModal = document.querySelector(".modalImage");
     const overlay = document.querySelector(".overlay");
-    overlay.innerHTML = `<img src="${jsonResults.better_featured_image.source_url}" />`;
+    overlay.innerHTML = `<img class="popUpImg" src="${jsonResults.better_featured_image.source_url}" />`;
     openModal.onclick = function () {
       overlay.classList.remove("hide");
-      console.log("hallo");
     };
     overlay.onclick = function () {
       overlay.classList.add("hide");
@@ -40,42 +39,3 @@ async function getBlogposts(Id) {
   }
 }
 getBlogposts(id);
-
-// Overlay Popup
-
-// const openModalButtons = document.querySelectorAll('[data-modal-target]')
-// const closeModalButtons = document.querySelectorAll('[data-close-button]')
-// const overlay = document.getElementById('overlay')
-
-// openModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = document.querySelector(button.dataset.modalTarget)
-//     openModal(modal)
-//   })
-// })
-
-// overlay.addEventListener('click', () => {
-//   const modals = document.querySelectorAll('.modal.active')
-//   modals.forEach(modal => {
-//     closeModal(modal)
-//   })
-// })
-
-// closeModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = button.closest('.modal')
-//     closeModal(modal)
-//   })
-// })
-
-// function openModal(modal) {
-//   if (modal == null) return
-//   modal.classList.add('active')
-//   overlay.classList.add('active')
-// }
-
-// function closeModal(modal) {
-//   if (modal == null) return
-//   modal.classList.remove('active')
-//   overlay.classList.remove('active')
-// }
