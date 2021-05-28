af40c1af2f2bda7eb8b459e4bca357d1068c813b
trondtjemsland <69841794+trondtjemsland@users.noreply.github.com>
2021-05-28 22:12:35 +0200

    s

--- js/contact.js
+++ js/contact.js
@@ -1,49 +1,68 @@
-const submitBtn = document.querySelector("#submit");
+// async function getContatctForm() {
+//   const repsonse = await fetch(
+//     "https://noroffcors.herokuapp.com/https://api.tjemsland.online/wp-json/wp/v2/pages/"
+//   );
+//   const jsonFromServer = await repsonse.json();
+//   console.log(jsonFromServer);
 
-const nameError = document.querySelector(".nameError");
-const subjectError = document.querySelector(".subjectError");
-const emailError = document.querySelector(".emailError");
-const textError = document.querySelector(".textError");
+//   for (let i = 0; i < 1; i++) {
+//     console.log(jsonFromServer[i].content.rendered);
+//     document.querySelector("main").innerHTML += `
+//         ${jsonFromServer[i].content.rendered}
+//     `;
+//   }
+// }
+
+// getContatctForm();
+// const form = document.querySelector('#contactForm');
+// const submitBtn = document.querySelector('#submit');
+const nameError = document.querySelector('.nameError');
+const subjectError = document.querySelector('.subjectError');
+const emailError = document.querySelector('.emailError');
+const textError = document.querySelector('.textError');
 
 submitBtn.onclick = function (event) {
   event.preventDefault();
 
-  const name = document.querySelector("#name").value.trim();
-  const subject = document.querySelector("#subject").value.trim();
-  const email = document.querySelector("#email").value.trim();
-  const text = document.querySelector("#text").value.trim();
+  const name = document.querySelector('#name').value.trim();
+  const subject = document.querySelector('#subject').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const text = document.querySelector('#text').value.trim();
 
   //Trim  is working
 
-  if (name.length > 10) {
-    nameError.classList.add("hide");
-    nameError.classList.remove("show");
+  if (name.length > 4) {
+    nameError.classList.add('hide');
+    nameError.classList.remove('show');
   } else {
-    nameError.classList.add("show");
-    nameError.classList.remove("hide");
+    nameError.classList.add('show');
+    nameError.classList.remove('hide');
   }
   if (subject.length >= 8) {
-    subjectError.classList.add("hide");
-    subjectError.classList.remove("show");
+    subjectError.classList.add('hide');
+    subjectError.classList.remove('show');
   } else {
-    subjectError.classList.add("show");
-    subjectError.classList.remove("hide");
+    subjectError.classList.add('show');
+    subjectError.classList.remove('hide');
   }
   if (text.length >= 8) {
-    textError.classList.add("hide");
-    textError.classList.remove("show");
+    textError.classList.add('hide');
+    textError.classList.remove('show');
   } else {
-    textError.classList.add("show");
-    textError.classList.remove("hide");
+    textError.classList.add('show');
+    textError.classList.remove('hide');
   }
 
   if (validateEmail(email)) {
-    emailError.classList.add("hide");
-    emailError.classList.remove("show");
+    emailError.classList.add('hide');
+    emailError.classList.remove('show');
   } else {
-    emailError.classList.add("show");
-    emailError.classList.remove("hide");
+    emailError.classList.add('show');
+    emailError.classList.remove('hide');
   }
+
+  let data = new FormData(form);
+  console.log(data.get('email'));
 };
 
 function validateEmail(emailAddy) {
--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,33 @@
+async function getSlider() {
+  try {
+    const response = await fetch(
+      "https://api.tjemsland.online/wp-json/wp/v2/posts/"
+    );
+
+    const jsonResults = await response.json();
+    const postArray = jsonResults;
+    postArray.forEach(function (element) {
+      if (element.better_featured_image.source_url) {
+        document.querySelector("#charouselPosts").innerHTML += `
+        <div class="slide">
+        <a  href="specificblog.html?id=${element.id}"><img src="${element.better_featured_image.source_url}">
+            <div class="sliderCardtitle">
+                <h2 class="charouseltitle">${element.title.rendered}</h2>
+            </div>
+         </a>   
+        </div>
+        `;
+      } else {
+      }
+    });
+    charousel();
+  } catch (error) {
+  } finally {
+  }
+}
+
+getSlider();
+
 function charousel() {
   $(".blogs").slick({
     arrows: false,
@@ -25,38 +55,3 @@ function charousel() {
     ],
   });
 }
-
-async function getSlider() {
-  try {
-    const response = await fetch(
-      "https://api.tjemsland.online/wp-json/wp/v2/posts"
-    );
-
-    const jsonResults = await response.json();
-    const postArray = jsonResults;
-    postArray.forEach(function (element) {
-      if (element.better_featured_image.source_url) {
-        document.querySelector("#charouselPosts").innerHTML += `
-        <div class="slide">
-        <a  href="specificblog.html?id=${element.id}"><img src="${element.better_featured_image.source_url}">
-            <div class="sliderCardtitle">
-                <h2 class="charouseltitle">${element.title.rendered}</h2>
-            </div>
-         </a>   
-        </div>
-        `;
-      } else {
-        document.querySelector("#charouselPosts").innerHTML += `
-            <div class="h2undertitle">   
-                <h2>${element.title.rendered}</h2>
-            </div>
-        `;
-      }
-    });
-    charousel();
-  } catch (error) {
-  } finally {
-  }
-}
-
-getSlider();
